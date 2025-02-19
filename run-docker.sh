ff4795160e8e4032834a5e37f4b99040d7f7ab18
Linus Hemingway Kromwell <lhk@lhk.me>
2025-02-18 22:46:55 -0500

    Fully build the site in the Dockerfile, provide scripts, and fix gh pages flow

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,5 @@ echo "Deleting all old containers..."
 
 docker rm -v -f $(docker ps -qa)
 
-docker run -it -p 3000:3000 --platform linux/amd64 --volume=/home/lhk/plenti:/home plenty
+# docker run -it -p 3000:3000 --platform linux/amd64 megan-andrew-builder:latest
+docker run -it -p 3000:3000 --platform linux/amd64 megan-andrew:latest
