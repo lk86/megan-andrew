ff2af339c1b08df4b103e1266ae3cfc6e37818f0
lk86 <lk86@users.noreply.github.com>
2025-02-20 20:11:57 +0000

    Deploying to gh-pages from  @ ae6c3cfce14ba9eba9b94121c74cf59d749cc47a 🚀

--- spa/generated/env.js
+++ spa/generated/env.js
@@ -1,1 +1,1 @@
-export let env={local:!1,baseurl:"",routes:{pages:"/:filename"},types:["pages"],singleTypes:[],fingerprint:"SQbWKXxNTi",entrypointHTML:"global/html.svelte",entrypointJS:"spa",cms:{provider:"",repo:"",redirectUrl:"",appId:"",branch:""}}
+export let env={local:!1,baseurl:"",routes:{pages:"/:filename"},types:["pages"],singleTypes:[],fingerprint:"hIiRedrjNM",entrypointHTML:"global/html.svelte",entrypointJS:"spa",cms:{provider:"",repo:"",redirectUrl:"",appId:"",branch:""}}
